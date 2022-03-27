63a8a69584ede1f6956c51254808d5fc99749c85
yanjie <yanjie995@126.com>
2022-03-27 19:49:24 +0800

    测试2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
-        Log.d("yannnn,","textttt")
+        Log.d("yannnn,","texttxx")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
